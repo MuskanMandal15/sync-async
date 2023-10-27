98930b686f2587b11746762dea8b4994c1a25fd2
muskanmandal <mandalmuskan12gmail.com>
2023-10-27 20:28:38 +0530

    added index.html, async-sync.js file

---
+++ async-sync.js
@@ -0,0 +1,10 @@
+// let a = prompt("what's your name?")
+// let b = prompt("what's your age?")
+// let c = prompt("what's your favorite color?")
+// console.log(a + " is " + b + " years old has " + c + " favorite color.");
+
+console.log("start")
+setTimeout(function(){
+    console.log("hiii, i'm good")
+}, 3000);
+console.log("end");
