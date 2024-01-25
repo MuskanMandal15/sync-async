1baf351e85e60b1c648c02005c94049d3b7f8e42
muskanmandal <mandalmuskan12gmail.com>
2024-01-25 18:09:18 +0530

    sync-async

--- async-sync.js
+++ async-sync.js
@@ -1,10 +1,18 @@
+const BUTTON = document.querySelector("button");
+
+function userHandler() {
+  console.log("clicked!");
+}
+
+BUTTON.addEventListener("click", userHandler);
+
 // let a = prompt("what's your name?")
 // let b = prompt("what's your age?")
 // let c = prompt("what's your favorite color?")
 // console.log(a + " is " + b + " years old has " + c + " favorite color.");
 
-console.log("start")
-setTimeout(function(){
-    console.log("hiii, i'm good")
+console.log("start");
+setTimeout(function () {
+  console.log("hiii, i'm good");
 }, 3000);
 console.log("end");
